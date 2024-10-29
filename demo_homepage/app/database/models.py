2981008257b742bd78f9d4c3d582e373d22dd8e9
TaeryungIm <77139152+TaeryungIm@users.noreply.github.com>
2024-10-30 01:28:10 +0900

    10.30 first commit
    문의 작성을 위해 데이터베이스 테이블 셋업만 간단하게 해보았다

--- demo_homepage/app/database/models.py
+++ demo_homepage/app/database/models.py
@@ -1,14 +1,32 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
 from .database import Base
 
 
 class UserTable(Base):
-    __tablename__ = "demo_users"  # Table name
+    __tablename__ = "demo_users"  # User Table name
 
-    userid = Column(String(50), primary_key=True)  # Specify length for VARCHAR
+    # account info
+    id = Column(Integer, primary_key=True, index=True)
+    userid = Column(String(50), nullable=False)  # Specify length for VARCHAR
     username = Column(String(100), nullable=False)  # VARCHAR with length
     email = Column(String(100), unique=True, nullable=False)  # VARCHAR with length
     password = Column(String(100), nullable=False)  # VARCHAR with length
     gender = Column(String(10), nullable=False)
     age = Column(Integer, nullable=False)
 
+    demo_inquiries = relationship("InquiryTable", back_populates="demo_user")
+
+
+class InquiryTable(Base):
+    __tablename__ = "demo_inquiries" # Inquiry Table name
+
+    # inquiry info
+    id = Column(Integer, primary_key=True, index=True)
+    inquiry_title = Column(String(50), nullable=False)
+    inquiry_content = Column(String(500), nullable=False)
+
+    # time info
+    created_at = Column(DateTime, default=datetime.utcnow())
+    updated_at = Column(DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
